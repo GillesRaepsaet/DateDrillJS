0cd8bffc9f018684ba154d9376cd64bdb9ffe476
GillesRaepsaet <gillesr6000@hotmail.com>
2022-05-15 18:19:28 +0200

    Clock

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert()
+setInterval(setClock, 1000);
+const hours = document.querySelector('[Date-hours]')
+const minutes = document.querySelector('[Date-minutes]')
+const seconds = document.querySelector('[Date-seconds]')
+
+
+const setClock =  () => {
+    const currentDateRotation = new Date()
+    const currentSeconds = currentDateRotation.getSeconds()/60
+    const currentMinutes =(currentMinutes + currentDateRotation.getMinutes()/60)
+    const currentHours = (currentHours + currentDateRotation.getHours()/60)
+    setRotationClock(hours, currentHours)
+    setRotationClock(minutes, currentMinutes)
+    setRotationClock(seconds, currentSeconds)
+}
+
+const setRotationClock = (elm, rotationRatioDate) => {
+    elm.style.setProperty('--rotation', rotationRatioDate*360)
+}
+
+setClock()
